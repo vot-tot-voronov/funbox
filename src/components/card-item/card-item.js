6cdffebfea16e13a238214218d9cba5661f732cd
Ivan <frontenddeveloperweb@gmail.com>
2020-11-23 13:52:24 +0500

    Ended all styles

--- src/components/card-item/card-item.js
+++ src/components/card-item/card-item.js
@@ -1,12 +1,26 @@
-import React, { useState, useEffect, useMemo } from 'react'
+import React, { useState } from 'react'
 import './card-item.css';
 import cat from '../../images/funbox_logo.png';
 
 const CardItem = ({card}) => {
     const {withWhat, partials, presents, weight, description, availability} = card;
+    
+    const buy = () => {
+        setSelected(!selected)
+        if (!availability) {
+            setDescrParagr(paragraphAvl)
+        } else {
+            setDescrParagr(paragraphSelected);
+        }
+        setWarp_sel('wrapper_sel');
+        setContent_cercl_sel('content__circle_sel');
+        setSelhov(true);
+    }
     const paragraph = (
         <p className="descr__text">
-            Чего сидишь? Порадуй котэ, <span className="descr__text_btn">купи</span>.
+            Чего сидишь? Порадуй котэ, <span 
+                                            onClick={buy}
+                                            className="descr__text_btn">купи</span>.
         </p>
     );
     const paragraphSelected = (
@@ -14,42 +28,83 @@ const CardItem = ({card}) => {
             {description}
         </p>
     );
+    const paragraphAvl = (
+        <p className="descr__text descr__text_avl">
+            Печалька, {withWhat} закончился.
+        </p>
+    );
 
     const [selected, setSelected] = useState(false);
-    const [descrParagr, setdescrParagr] = useState(paragraph);
+    const [descrParagr, setDescrParagr] = useState(paragraph);
+    const [warp_sel, setWarp_sel] = useState('wrapper_hov');
+    const [content_cercl_sel, setContent_cercl_sel] = useState('content__circle_hov');
+
+    const [signature, setSignature] = useState('Сказочное заморское яство');
+    const [selhov, setSelhov] = useState(false);
+    const [signColor, setSignColor] = useState('');
+
+    const [contentTextDis, setContentTextDis] = useState('');
+    const [logoOpacity, setLogoOpacity] = useState('');
 
     const changeParagr = () => {
         if (selected) {
-            setdescrParagr(paragraphSelected);
+            if (!availability) {
+                setDescrParagr(paragraphAvl);
+                setWarp_sel('wrapper_sel_avl');
+                setContent_cercl_sel('content__circle_sel_avl');
+                setContentTextDis('content_text_dis');
+                setLogoOpacity('logo_opacity');
+            } else {
+                setDescrParagr(paragraphSelected);
+                setWarp_sel('wrapper_sel');
+                setContent_cercl_sel('content__circle_sel');
+                setSelhov(true);
+            }
+            
         } else {
-            setdescrParagr(paragraph);
+            setDescrParagr(paragraph);
+            setWarp_sel('wrapper_hov');
+            setContent_cercl_sel('content__circle_hov');
+            setSelhov(false);
+            setContentTextDis('');
+            setLogoOpacity('');
+        }
+        setSignature('Сказочное заморское яство');
+        setSignColor('');
+    }
+    const changeSignature = () => {
+        if (selhov) {
+            if (availability) {
+                setSignature('Котэ не одобряет?');
+                setSignColor('signature_sel_hov');
+            }
         }
     }
     
     return (
         <div className="card">
-            <div className="wrapper">
-                <div onClick={()=> setSelected(!selected)} onMouseLeave={changeParagr} className="content">
-                    <div className="content__text">
-                        <p className="signature">Сказочное заморское яство</p>
-                        <h2 className="title">Нямушка</h2>
-                        <h3 className="name">{withWhat}</h3>
+            <div className={`wrapper ${warp_sel}`}>
+                <div 
+                    onClick={()=> setSelected(!selected)} 
+                    onMouseLeave={changeParagr} 
+                    onMouseMove={changeSignature}
+                    className="content">
+                    <div className={`content__text ${contentTextDis}`}>
+                        <p className={`signature ${signColor}`}>{signature}</p>
+                        <h2 className={`title ${contentTextDis}`}>Нямушка</h2>
+                        <h3 className={`name ${contentTextDis}`}>{withWhat}</h3>
                         <p className="portion">{partials}</p>
                         <p className="present">{presents}</p>
                     </div>
-                    <img src={cat} alt="Funbox Cat" className="content__logo"/>
-                    <div className='content__circle'>
+                    <img src={cat} alt="Funbox Cat" className={`content__logo ${logoOpacity}`}/>
+                    <div className={`content__circle ${content_cercl_sel}`} >
                         <p className='kilo'>{weight}<br/><span>кг</span></p>
                     </div>
                 </div>
             </div>
             {descrParagr}
-            {/* <p className="descr__text">
-                Чего сидишь? Порадуй котэ, <span className="descr__text_btn">купи</span>.
-            </p> */}
         </div>
     );
-
 }
 
 export default CardItem;
